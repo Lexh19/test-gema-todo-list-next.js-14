fef26499f0032375a46c84fcbf0bfff73cbe51e2
Lexh19 <iqbaalhzd@gmail.com>
2024-10-08 20:16:36 +0700

    hhtps gema test

--- src/modules/axios/index.js
+++ src/modules/axios/index.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 require("dotenv").config();
 
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL || "https://gema-test.vercel.app/api";
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL || "https://test-gema.vercel.app/api";
 
 const instance = axios.create({ baseURL });
 
